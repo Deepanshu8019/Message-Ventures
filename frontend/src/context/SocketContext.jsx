e0c8e9ae15ede6eb92050d9925791c210be35bfb
Deepanshu8019 <deepanshushuka2723@gmail.com>
2024-07-08 22:28:54 +0530

    first commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			// const socket = io("https://chat-app-yt.onrender.com", {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://message-ventures.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
